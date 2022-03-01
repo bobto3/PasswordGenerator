92a05512ad43dadfe27d11d76bf5ccebb55b8206
Robert Anthony <rwanthony3@gmail.com>
2022-03-01 09:51:31 -0800

    added .concat and for loop)

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// var inclCapitalLetters = False;
-// var inclLowerCaseLetters = False;
-// var inclNumbers = False;
-// var inclSpecialCharacters = False;
-
-var capitalLetters = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
-var lowerCaseLetters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
-var numbers = ["1,2,3,4,5,6,7,8,9,0"];
-var specialCharacters = ["!,@,#,$,%,^,&,*,(,),-,+"];
+var capitalLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCaseLetters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",");
+var numbers = "1,2,3,4,5,6,7,8,9,0".split(",");
+var specialCharacters = "!,@,#,$,%,^,&,*,(,),-,+".split(",");
 
 function validateCharsLength (numOfCharacters) {
   if (numOfCharacters < 8 || numOfCharacters > 128) {
     confirm("Please enter a number of characters between 8 and 128");
     return generatePassword();
   } 
+  return numOfCharacters;
 }
 
 function generatePassword() {
+  var characterArray = [];
   var numOfCharacters = prompt("How many characters would you like your password to be?");
   validateCharsLength(numOfCharacters);
   var inclCapitalLetters = confirm("Would you like to use capital letters?");
@@ -23,12 +20,36 @@ function generatePassword() {
   var inclNumbers = confirm("Would you like to use numbers?");
   var inclSpecialCharacters = confirm("Would you like to use special characters?");
   
+  function validateSomeCharsIncl () {
+    if (!inclCapitalLetters && !inclLowerCaseLetters && !inclNumbers && !inclSpecialCharacters) {
+      confirm("Error! Please select at least one character type.");
+      return generatePassword();
+    } 
+    }
+  validateSomeCharsIncl();
 
-  if (inclCapitalLetters || inclLowerCaseLetters || inclNumbers || inclSpecialCharacters) {
-    document.querySelector(".card-body").innerHTML=("test");
-  } else {
-    document.querySelector(".card-body").innerHTML=("Error! Please select at least one character type.");
+  if (inclCapitalLetters) {
+    characterArray = characterArray.concat(capitalLetters);
   }
+  if (inclLowerCaseLetters) {
+    characterArray = characterArray.concat(lowerCaseLetters);
+  }
+  if (inclNumbers) {
+    characterArray = characterArray.concat(numbers);
+  }
+  if (inclSpecialCharacters) {
+    characterArray = characterArray.concat(specialCharacters);
+  }
+  console.log(characterArray);
+  console.log(numOfCharacters);
+
+  var finalString = "";
+
+  for (var i =0; i < parseInt(numOfCharacters); i++) {
+    var randomIndex = Math.floor(Math.random()*characterArray.length);
+    finalString += characterArray[randomIndex];
+  }
+  return finalString;
 }
 
 
